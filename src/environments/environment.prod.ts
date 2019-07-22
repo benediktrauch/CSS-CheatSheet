fcf32d56214b9b5e0228df6b329fdd6d080c50b2
benediktrauch <benediktrauch@users.noreply.github.com>
2019-07-22 16:49:45 +0200

    updated env settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBOtkPNX_eWy919hzkKk0q26T7SWvIGNrQ',
+    authDomain: 'css-cheat-sheet.firebaseapp.com',
+    databaseURL: 'https://css-cheat-sheet.firebaseio.com',
+    projectId: 'css-cheat-sheet',
+    storageBucket: '',
+    messagingSenderId: '332722619447',
+    appId: '1:332722619447:web:50b52fead6c7699d'
+  }
 };
